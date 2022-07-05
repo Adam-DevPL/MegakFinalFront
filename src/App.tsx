1a71990d5e764bf4c747c1e9502a2915db5c939e
Adam Wojnarowski <adam.piotr.wojnarowski@gmail.com>
2022-07-05 17:20:07 +0200

    Add all needed components and some function as add task, delete task, add project.

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,19 @@
 import React from 'react';
 import "./index.css"
+import {Route, Routes} from "react-router-dom";
+import {HomeView} from "./views/HomeView";
+import {NotFoundView} from "./views/NotFoundView";
 
-export const App = () => (
-    <div className="main-container">
-        <div className="all-tasks">
-            <h2 className="taks-list-title">Projects</h2>
-            <ul className="task-list">
-                <li className="active-list">Youtube</li>
-                <li>Work</li>
-                <li>Grocery</li>
-            </ul>
+export const App = () => {
+    return (
+        <>
+            <Routes>
+                <Route path="/" element={<HomeView/>}/>
+                <Route path="*" element={<NotFoundView/>}/>
+            </Routes>
+        </>
+    )
+};
+// <">
 
-            <form action="">
-                <input type="text" className="new list" placeholder="new project name" aria-label="create new list"/>
-                <button className="btn list">+</button>
-            </form>
-        </div>
-
-        <div className="todo-list">
-            <h2 className="todo-header">TO-DO</h2>
-            <form action="" className="form-container">
-                <input type="text" className="input" placeholder='Task to be done...'/>
-                <button className="submit-btn">Add</button>
-            </form>
-            <div className="todo-container">
-                <input type="checkbox" className="check-box"/>
-                <p className="task-text">Filmik</p>
-                <button className="edit-todo">✎</button>
-                <button className="remove-todo">Ⅹ</button>
-            </div>
-            <div className="todo-container">
-                <input type="checkbox" className="check-box"/>
-                <p className="task-text">WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW</p>
-                <button className="edit-todo">✎</button>
-                <button className="remove-todo">Ⅹ</button>
-            </div>
-            <button className="delete-list">Delete ALL</button>
-        </div>
-
-    </div>
-);
+// </div>
